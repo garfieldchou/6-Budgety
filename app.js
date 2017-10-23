6b5abb01c3b8f0bb97019dd40c7c12a69e5c4869
shaudai <shaudai0402@hotmail.com>
2017-10-23 22:56:01 +0800

    remove test code and add a event listener to controller

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
     
-    var x = 23;
-    
-    var add = function(a) {
-        return x + a;
-    }
-    
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
-    
+    // Some code
+
 })();
 
 
+// UI CONTROLLER
 var UIController = (function() {
     
     // Some code
@@ -22,14 +14,12 @@ var UIController = (function() {
 })();
 
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
     
-    var z = budgetCtrl.publicTest(5);
-    
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
-    }
+    document.querySelector('.add__btn').addEventListener('click', function(){
+        console.log('Button was clicked.');
+    });
+
     
 })(budgetController, UIController);
