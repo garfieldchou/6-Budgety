153e4e74930810d0cb56ebed1795501350e271f7
shaudai <shaudai0402@hotmail.com>
2017-10-24 06:53:46 +0800

    get the input values from the UI controller

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     var ctrlAddItem = function() {
         
         // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
         
         // 2. Add the item to the budget controller
         
@@ -36,8 +38,6 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
         
         // 5. Display the budget on the UI
-        
-        console.log('It works.');
 
     }
     
