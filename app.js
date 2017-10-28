5fa3258d376d766de45850828800eab81ae15093
shaudai <shaudai0402@hotmail.com>
2017-10-28 22:34:26 +0800

    correct the ID to int type for index

--- app.js
+++ app.js
@@ -263,7 +263,7 @@ var controller = (function(budgetCtrl, UICtrl) {
             //inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. delete the item from the data structure
             budgetCtrl.deleteItem(type, ID);
