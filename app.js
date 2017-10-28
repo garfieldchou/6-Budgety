6990c8ac647db87f823a01d7bd9b03c00763e686
shaudai <shaudai0402@hotmail.com>
2017-10-28 22:59:15 +0800

    prepare the function to update the percentage

--- app.js
+++ app.js
@@ -237,6 +237,16 @@ var controller = (function(budgetCtrl, UICtrl) {
         UICtrl.displayBudget(budget);
     };
 
+    var updatePercentages = function() {
+
+        // 1. Calculate percentages
+
+        // 2. Read percentages from the budget controller
+
+        // 3. Update the UI with the new percentages
+
+    };
+
     var ctrlAddItem = function() {
         
         var input, newItem;
@@ -256,6 +266,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 5. Calculate and update budget
             updateBudget();
+
+            // 6. Calculate and update percentages
+            updatePercentages();
         }
 
     };
@@ -280,6 +293,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 3. Update and show the new budget
             updateBudget();
+
+            // 4. Calculate and update percentages
+            updatePercentages();
         }
 
     };
