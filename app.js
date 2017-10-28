abf37dd52686c23ebf749364dd055f3524363b9c
shaudai <shaudai0402@hotmail.com>
2017-10-28 17:35:07 +0800

    create a method for deleting the item with correct index

--- app.js
+++ app.js
@@ -63,6 +63,26 @@ var budgetController = (function() {
             return newItem;
         },
         
+        deleteItem: function(type, id) {
+            var ids, index;
+
+            // id = 6
+            //data.allItems[type];
+            //ids = [1 2 4 6 8]
+            //index = 3
+
+            var ids = data.allItems[type].map(function(current){
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+
+        },
+
         calculateBudget: function() {
 
             // calculate total income and expenses
