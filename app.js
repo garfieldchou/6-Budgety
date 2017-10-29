919915763c998c02dbd04ea51594763ecdaf3a13
shaudai <shaudai0402@hotmail.com>
2017-10-29 10:54:54 +0800

    update each item's percentage with forEach function for node list

--- app.js
+++ app.js
@@ -239,7 +239,22 @@ var UIController = (function() {
 
         displayPercentages: function(percentages) {
 
-            var fields = document.querySelectorAll(DOMstrings.expensesLabel);
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function(current, index) {
+
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            });
 
         },
 
@@ -290,7 +305,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var percentages = budgetCtrl.getPercentages();
 
         // 3. Update the UI with the new percentages
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     };
 
     var ctrlAddItem = function() {
